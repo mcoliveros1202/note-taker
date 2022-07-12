c203e6e5bedb84b92d9c5a7810e18d936c953c59
moliveros <mcoliveros1202@gmail.com>
2022-07-11 20:27:51 -0500

    create post route to add note to notes array and validated data posted

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+const fs = require('fs');
+const path = require('path');
 const express =  require('express');
 const { notes } = require('./db/db.json');
+const { create } = require('domain');
 const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 // filter searches-- is this necessary for this app?
 function filterByQuery(query, notesArray) {
@@ -19,6 +27,29 @@ function findById(id, notesArray) {
   const result = notesArray.filter(note => note.id === id)[0];
   return result;
 }
+
+// creates new note in the notes array
+function createNewNote(body, notesArray) {
+  const note = body;
+
+  notesArray.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, './db/db.json'),
+    JSON.stringify({ notes: notesArray }, null, 2)
+  );
+  // return finished code to post route for response
+  return note;
+}
+
+function validateNote(note) {
+  if (!note.title || typeof note.title !== 'string') {
+    return false;
+  }
+  if (!note.text || typeof note.text !== 'string') {
+    return false
+  }
+  return true;
+};
 
 // calls db.json
 app.get('/api/notes', (req, res) => {
@@ -40,9 +71,18 @@ app.get('/api/notes/:id', (req, res) => {
 });
 
 app.post('/api/notes', (req, res) => {
-  // req.body is where incoming content will be
-  console.log(req.body);
-  res.json(req.body);
+  // set id based on what the next index of the array will be
+  req.body.id = notes.length.toString();
+
+  // if any data in req.body is incorrect, send 400 error back
+  if(!validateNote(req.body)) {
+    res.status(400).send('The note is not properly formatted.');
+  } else {
+    // add note to json file and notes array in this function
+
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+  }
 });
 
 app.listen(3001, () => {
