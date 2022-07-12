f8b1e105b123c78ec9bdb4cc0ba0ec4b8bb4287d
moliveros <mcoliveros1202@gmail.com>
2022-07-11 22:01:29 -0500

    modularized code

--- server.js
+++ server.js
@@ -1,63 +1,20 @@
-const fs = require('fs');
-const path = require('path');
 const express =  require('express');
-const { notes } = require('./db/db.json');
-const { create } = require('domain');
-const app = express();
 
 // for heroku
 const PORT = process.env.PORT || 3001
-
-app.use(express.static('public'));
+const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
-
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
-// calls db.json
-app.get('/api/notes', (req, res) => {
-  let results = notes;
-  if (req.query) {
-    results = filterByQuery(req.query, results);
-  }
-  res.json(results);
-});
-
-// allows filtering search through id
-app.get('/api/notes/:id', (req, res) => {
-  const result = findById(req.params.id, notes);
-  if (result) {
-    res.json(result)
-  } else {
-    res.send(404)
-  }
-});
-
-app.post('/api/notes', (req, res) => {
-  // set id based on what the next index of the array will be
-  req.body.id = notes.length.toString();
-
-  // if any data in req.body is incorrect, send 400 error back
-  if(!validateNote(req.body)) {
-    res.status(400).send('The note is not properly formatted.');
-  } else {
-    // add note to json file and notes array in this function
-
-    const note = createNewNote(req.body, notes);
-    res.json(note);
-  }
-});
-
-// HTML routes
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/notes.html'))
-})
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-});
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 // listen for server
 app.listen(3001, () => {
